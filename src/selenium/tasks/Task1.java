7babe3792593eca6decb90d1ae83acc3793c1691
YANA LIK <yana.v.lik@gmail.com>
2022-04-01 18:02:35 +0300

    task 1

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,10 +3,14 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import static org.junit.Assert.*;
 
 public class Task1 {
     WebDriver driver;
@@ -26,15 +30,25 @@ public class Task1 {
     }
 
     @Test
-    public void errorOnText() {
+    public void errorOnText() throws InterruptedException {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        String textToEnter = "someText";
+        driver.findElement(By.id("numb")).sendKeys(textToEnter);
+        driver.findElement(By.tagName("button")).click();
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        assertEquals("Please enter a number", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        String smallNumberToEnter = "45";
+        driver.findElement(By.id("numb")).sendKeys(smallNumberToEnter);
+        driver.findElement(By.tagName("button")).click();
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        assertEquals("Number is too small", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
@@ -43,6 +57,11 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        String bigNumberToEnter = "455";
+        driver.findElement(By.id("numb")).sendKeys(bigNumberToEnter);
+        driver.findElement(By.tagName("button")).click();
+        System.out.println(driver.findElement(By.id("ch1_error")).getText());
+        assertEquals("Number is too big", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
@@ -50,6 +69,22 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        String validNumberToEnter = "64";
+        driver.findElement(By.id("numb")).sendKeys(validNumberToEnter);
+        driver.findElement(By.tagName("button")).click();
+
+        Alert correctAlert = driver.switchTo().alert();
+        String expectedMessage = "Square root of 64 is 8.00";
+        String actualMessage = correctAlert.getText();
+        assertEquals(expectedMessage,actualMessage);
+        correctAlert.accept();
+
+        String expectedErrorMessage = "";
+        String actualErrorMessage = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals(expectedErrorMessage,actualErrorMessage);
+
+
+
     }
 
     @Test
@@ -57,5 +92,20 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        String validNumberToEnter = "65";
+        driver.findElement(By.id("numb")).sendKeys(validNumberToEnter);
+        driver.findElement(By.tagName("button")).click();
+
+
+        Alert correctAlert = driver.switchTo().alert();
+        String expectedMessage = "Square root of 65 is 8.06";
+        String actualMessage = correctAlert.getText();
+        assertEquals(expectedMessage,actualMessage);
+        correctAlert.accept();
+
+        String expectedErrorMessage2 = "";
+        String actualErrorMessage2 = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals(expectedErrorMessage2,actualErrorMessage2);
+
     }
 }
